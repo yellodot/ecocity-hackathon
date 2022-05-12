cd022c794746b68d2c5c53631aac2aec72ad4886
Gregoria R <rubio.gregoria@gmail.com>
2022-05-12 15:57:49 +0200

    changed font-size

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
     <div className='flex flex-col justify-center items-center bg-emerald-500 w-screen h-screen'>
-      <div className='flex justify-center items-center text-4xl font-bold text-slate-900 text-center my-8 w-2/5'>
+      <div className='flex justify-center items-center text-6xl font-bold text-slate-900 text-center py-12 my-14 w-2/3'>
         Trouvez la ville proche de vos préoccupations écologiques avec EcoCity.fr
       </div>
       <SearchBar />
