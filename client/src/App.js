cb7062ba31f223b355b50a6c77256888e6b764ac
Stefannee <stephanie.caussier@gmail.com>
2022-05-12 18:40:47 +0200

    init page detail

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from './components/Home';
-
+import Home from "./components/Home";
+import CityDetails from "./components/CityDetails";
 
 function App() {
   return (
-    <div className='flex w-screen'>
+    <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/city-details" element={<CityDetails />} />
       </Routes>
     </div>
   );
