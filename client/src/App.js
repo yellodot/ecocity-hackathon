f219b874ea598453aada2798009343188aaf8ca0
Stefannee <stephanie.caussier@gmail.com>
2022-05-12 21:47:01 +0200

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
 import CityDetails from "./components/CityDetails";
-import Home from './components/Home';
+import Home from "./components/Home";
 import Header from "./components/header/Header.jsx";
-
 
 function App() {
   return (
-     <>
-       <Header/>
-    <div>
-           <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/city-details" element={<CityDetails />} />
-      </Routes>
-    </div>
-
+    <>
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/city-details" element={<CityDetails />} />
+        </Routes>
+      </div>
     </>
   );
 }
