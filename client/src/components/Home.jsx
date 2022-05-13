99e774e624f12626d43dfe3659b9c767635a73dd
Gregoria R <rubio.gregoria@gmail.com>
2022-05-13 10:59:19 +0200

    feat: finished filters

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import FiltersCopy from './FiltersCopy'
+import Filters from './Filters'
 import SearchBar from './SearchBar'
 
 function Home() {
   return (
     <>
-    <div className='flex flex-col justify-center items-center bg-emerald-500 w-screen h-screen'>
+    <div className='flex flex-col justify-center items-center bg-emerald-500 w-screen'>
       <div className='flex justify-center items-center text-6xl font-bold text-slate-900 text-center py-12 my-14 w-2/3'>
         Trouvez la ville proche de vos préoccupations écologiques avec EcoCity.fr
       </div>
       <SearchBar />
-      <FiltersCopy />
+      <Filters />
     </div>
     </>
   )
