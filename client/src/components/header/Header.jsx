7cf030fc6129baca024bdcd49efdf3df221449f3
Marie T <marie.toupuissant@gmail.com>
2022-05-13 09:53:47 +0200

    link nav bar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "../header/Header.css";
 import eco from "../../assets/images/eco.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="Header">
         <div className="container-header">
+          <Link to ="/Home">
           <img src={eco} alt={eco} />
+          </Link>
+          <Link to ="/Contact">
         <button className="button-contact">Contactez-nous ! </button>
+        </Link>
       </div>
     </div>
   )
 }
-
 
 export default Header;
