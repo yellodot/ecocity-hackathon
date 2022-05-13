dac4a448dc080c75e724c200529a427d14f6daba
Eric <pradier_eric@orange.fr>
2022-05-13 11:16:30 +0200

    citycard score

--- client/src/components/FiltersCopy.jsx
+++ client/src/components/FiltersCopy.jsx
@@ -138,7 +138,7 @@ function Filters() {
               {
                 filteredCities?.map((city) => 
                 <li className='pb-6'>
-                  <CityCard nom={city.nom} score={city.score} dechets={city.traitement_dechet} espace={city.espace_vert} qualite={city.qualite_eau}  />
+                  <CityCard nom={city.nom} score={city.label.toLowerCase()==="vert" ? "80" : city.label.toLowerCase() === "orange" ? "60" : "40"} dechets={city.traitement_dechet} espace={city.espace_vert} qualite={city.qualite_eau}  />
                 </li>
                 )
               }
